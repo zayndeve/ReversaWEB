ffa4067ba1b54f9be30e4b0db7ca1f00206269b8
Ziynatilloh <ziynatillokht@gmail.com>
2025-11-03 15:38:48 +0900

    modified: changes to be committed: 19

--- WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63886b9341649421359a2b240341a126a5fb25d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6138d935672ba66d03ef8e1e4e7d643766a8a286")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
